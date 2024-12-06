834fb1d609bf351f774895573fdecee6effac398
Hasan9797 <hasanjonsadullayev97@gmail.com>
2024-12-06 15:38:07 +0500

    start command

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramBotController extends Controller
 {
     public function handle(Request $request)
     {
-        $message = $request->get('message');
-        $chatId = $request->get('chat_id');
-        dd($request);
+        Log::info('Telegram Request:', $request->all()); // Request ma'lumotlarini logga yozib qo'yish
+
+        $message = $request->input('message.text');
+        $chatId = $request->input('message.chat.id');
+
         if ($message == '/start') {
             $reply = "Welcome to the Anvar Jigga Clean Service chatbot!.👋";
             Telegram::sendMessage([
