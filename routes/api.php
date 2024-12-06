8dcff5b6dd89a1a729598b84bd8d3498990e026c
Hasan9797 <hasanjonsadullayev97@gmail.com>
2024-12-06 16:25:40 +0500

    starttt

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\TelegramBotController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Log::info('This is a test log message!');
+    return 'This is a test log message';
+});
 
 Route::get('telegram/webhook', [TelegramBotController::class, 'handle']);
