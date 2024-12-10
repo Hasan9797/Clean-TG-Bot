7bfcf5c749bbdb6035b37be2b3cd598acdaf079a
Hasan9797 <hasanjonsadullayev97@gmail.com>
2024-12-10 11:34:24 +0500

    teee

--- app/Services/Telegram/Commands/ContactCommand.php
+++ app/Services/Telegram/Commands/ContactCommand.php
@@ -25,7 +25,6 @@ class ContactCommand
         if (isset($contact['phone_number'])) {
             $phoneNumber = $contact['phone_number'];
             $firstName = $contact['first_name'];
-            TelegramBotHelper::deleteMessage($chatId, $messageId);
             (new ServicesCommand())->execute($request);
             return true;
         }
