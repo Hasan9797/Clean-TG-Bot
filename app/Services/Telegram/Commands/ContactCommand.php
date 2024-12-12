0f412595c48be753511df92c7092851fcfdf85fe
Hasan9797 <hasanjonsadullayev97@gmail.com>
2024-12-12 13:00:36 +0500

    client create

--- app/Services/Telegram/Commands/ContactCommand.php
+++ app/Services/Telegram/Commands/ContactCommand.php
@@ -6,8 +6,7 @@ use App\Helpers\PhoneHelper;
 use App\Helpers\TelegramBotHelper;
 use App\Services\CacheService;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Cache;
-use Telegram\Bot\Objects\Contact;
+use Illuminate\Support\Facades\Log;
 
 class ContactCommand
 {
@@ -23,23 +22,30 @@ class ContactCommand
 
     public function execute($request)
     {
-        $chatId = $request->input('message.chat.id');
-        $messageId = $request->input('message.message_id');
-        $phoneNumber = Arr::get($request->input('message.contact'), 'phone_number', $request->input('message.text'));
 
-        if (PhoneHelper::isValidPhoneNumber($phoneNumber)) {
+        try {
+            $chatId = $request->input('message.chat.id');
+            $messageId = $request->input('message.message_id');
+            $phoneNumber = Arr::get($request->input('message.contact'), 'phone_number', $request->input('message.text'));
 
-            $response = (new ServicesCommand())->getServices($chatId, $messageId);
+            if (PhoneHelper::isValidPhoneNumber($phoneNumber)) {
 
-            if(!empty($response)){
-                CacheService::updateCache("contact_$chatId", $phoneNumber);
+                $response = (new ServicesCommand())->getServices($chatId, $messageId);
+
+                if (!empty($response)) {
+                    CacheService::updateCache("contact_$chatId", $phoneNumber);
+                }
+
+                return true;
             }
 
-            return true;
+            $message = 'Kontaktni yubormadingiz. Iltimos, tugmani bosing va kontakt yuboring.';
+
+            TelegramBotHelper::sendMessage($chatId, $message);
+        } catch (\Throwable $th) {
+            Log::error('Error: ' . $th->getMessage());
+            TelegramBotHelper::sendMessage($chatId, 'Xatolik yuz berdi: ' . $th->getMessage());
+            return false;
         }
-
-        $message = 'Kontaktni yubormadingiz. Iltimos, tugmani bosing va kontakt yuboring.';
-
-        TelegramBotHelper::sendMessage($chatId, $message);
     }
 }
